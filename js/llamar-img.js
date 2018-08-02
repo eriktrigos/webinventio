8943aec8f9fded1a45f9eac2010b1b92a316e7c5
eriktrigos <41607029+eriktrigos@users.noreply.github.com>
2018-08-02 12:55:46 -0500

    Imagenes
    Mejor organizacion de las imagenes por carpeta, se emepzó a trabajar en la plantilla de los proyectos del portafolio, se hicieron pruebas de las imagenes 360 sin mucho exito

---
+++ js/llamar-img.js
@@ -0,0 +1,17 @@
+$(function() {
+
+	//l'array ARR va contenir la liste des images
+	var arr = [];
+	//trop lazy pour tout taper... 
+	for (var x=1450; x<= 1480; x++)
+		arr.push("img/MAT_" +x + ".png");
+	var arr2 = []
+	for (var x=1; x<= 25; x++)
+		arr2.push(x + ".png");
+	
+	//bindons le widget rotate � l'image pic avec la liste d'image arr
+	$("#mousemove").threesixty({images:arr, method:'mousemove', 'cycle':3, direction:"backward"});
+	$("#click").threesixty({images:arr2, method:'click', 'cycle':1, 'resetMargin': 10});
+	$("#auto").threesixty({images:arr2, method:'auto', autoscrollspeed:100});
+
+});
